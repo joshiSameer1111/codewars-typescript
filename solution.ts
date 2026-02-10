fa91161b179ec4fea2a6bdb707eb707cb962b9e4
Sameer Joshi <joshisameer023@gmail.com>
2026-02-10 14:06:12 -0600

    Take a Ten Minutes Walk

--- solution.ts
+++ solution.ts
@@ -1,22 +1,31 @@
-export class Kata {
-  static squareDigits(num: number): number {
-    // let result = "";
-    // if (num == 0) {
-    //   return 0; // Handle the case where the input is 0
-    // }
-    // while (num > 0) {
-    //   const lastDigit = num % 10;
-    //   num = Math.floor(num / 10); // Everything but the last digit
-    //   const squaredDigit = lastDigit * lastDigit;
-    //   result = squaredDigit + result;
-    // }
-    // return parseInt(result); // Convert the string back to a number
+export function isValidWalk(walk: string[]) {
+  // const directions = {
+  //   n: 0,
+  //   s: 0,
+  //   w: 0,
+  //   e: 0
+  // };
+  // for (const direction of walk) {    
+  //   directions[direction as keyof typeof directions]++;
+  // }
+  // return walk.length == 10 && directions.n == directions.s && directions.w == directions.e;
 
-    return +num.toString()
-               .split('')
-               .map(n => Math.pow(+n,2))
-               .join('');
-  }
+  if (walk.length !== 10) return false;
 
-   
+  type directionValues = {
+    [key: string]: number;
+  };
+
+  const directions: directionValues = {
+    n: 0,
+    s: 0,
+    w: 0,
+    e: 0,
+  };
+
+  walk.forEach((direction: string) => {
+    directions[direction]++;
+  });
+
+  return directions.n === directions.s && directions.w === directions.e;
 }
