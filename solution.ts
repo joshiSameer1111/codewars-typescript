7cf3882b2fa817a549ff171969da22c71231252b
Sameer Joshi <joshisameer023@gmail.com>
2026-02-10 13:50:49 -0600

    Square Every Digit

--- solution.ts
+++ solution.ts
@@ -1,11 +1,22 @@
-export function howMuchILoveYou(petals: number): string{
-  const phrases = [
-    "I love you",
-    "a little",
-    "a lot",
-    "passionately",
-    "madly",
-    "not at all"
-  ];
-  return phrases[(petals - 1) % phrases.length];
+export class Kata {
+  static squareDigits(num: number): number {
+    // let result = "";
+    // if (num == 0) {
+    //   return 0; // Handle the case where the input is 0
+    // }
+    // while (num > 0) {
+    //   const lastDigit = num % 10;
+    //   num = Math.floor(num / 10); // Everything but the last digit
+    //   const squaredDigit = lastDigit * lastDigit;
+    //   result = squaredDigit + result;
+    // }
+    // return parseInt(result); // Convert the string back to a number
+
+    return +num.toString()
+               .split('')
+               .map(n => Math.pow(+n,2))
+               .join('');
+  }
+
+   
 }
