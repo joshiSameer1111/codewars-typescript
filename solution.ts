63d6f971b4bcd309025cab7f5a822cf03e0674ea
Sameer Joshi <joshisameer023@gmail.com>
2026-02-16 14:12:52 -0600

    Find The Parity Outlier

--- solution.ts
+++ solution.ts
@@ -1,3 +1,9 @@
-export function findSmallestInt(args: number[]): number {
-  return Math.min(...args);
+export function findOutlier(integers: number[]): number {
+  const even = integers.filter((x) => x % 2 == 0);
+  const odd = integers.filter((x) => x % 2 != 0);
+  if (even.length == 1) {
+    return even[0];
+  } else {
+    return odd[0];
+  }
 }
--- test.ts
+++ test.ts
@@ -1,12 +1,11 @@
-import {findSmallestInt} from "./solution";
-import {assert} from "chai";
+import { assert } from 'chai';
 
-describe("Smallest Integer Tests", function() {
-  it("Fixed Tests", function() {
-    assert.strictEqual(findSmallestInt([78,56,232,12,8]),8,'Should return the smallest int 8');
-    assert.strictEqual(findSmallestInt([78,56,232,12,18]),12,'Should return the smallest int 12');
-    assert.strictEqual(findSmallestInt([78,56,232,412,228]),56,'Should return the smallest int 56');
-    assert.strictEqual(findSmallestInt([78,56,232,12,0]),0,'Should return the smallest int 0');
-    assert.strictEqual(findSmallestInt([1,56,232,12,8]),1,'Should return the smallest int 1');
-  });
-});  
+import { findOutlier } from './solution';
+
+it('Simple tests', function() {
+  assert.equal(findOutlier([0, 1, 2]), 1);
+  assert.equal(findOutlier([1, 2, 3]), 2);
+  assert.equal(findOutlier([2, 6, 8, 10, 3]), 3);
+  assert.equal(findOutlier([0, 0, 3, 0, 0]), 3);
+  assert.equal(findOutlier([1, 1, 0, 1, 1]), 0);
+});
